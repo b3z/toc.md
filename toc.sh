a59680be16f9c96d299bab0eb84de29b270dc7bb
Luca <b3z@zeppel.eu>
2022-01-11 00:59:01 +0100

    Update toc.sh

--- toc.sh
+++ toc.sh
@@ -1,3 +1,3 @@
-function toc4 () {
+function toc () {
 function m () { grep -E "^#{${1:-1},${2:-2}} " | sed -E 's/(#+) (.+)/\1:\2:\2/g' | awk -F ":" '{ gsub(/#/,"  ",$1); gsub(/[ ]/,"-",$3); print $1 "- [" $2 "](#" tolower($3) ")" }'
 } && res=$(cat "$*"| m 1 10) && echo "$res" && echo "$res"| pbcopy && echo "\nCopied to clip." }
